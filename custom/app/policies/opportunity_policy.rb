0e3a0e05c63e6de76ec770da81755a7e6df77ba4
Edney Matias <edneymatias@gmail.com>
2026-08-08 11:47:51 -0300

    refactor(opportunities): temporarily grant all agents full access to all opportunities

--- custom/app/policies/opportunity_policy.rb
+++ custom/app/policies/opportunity_policy.rb
@@ -4,7 +4,7 @@ class OpportunityPolicy < ApplicationPolicy
   end
 
   def show?
-    administrator? || assignee? || conversation_access?
+    @account_user.present?
   end
 
   def create?
@@ -12,46 +12,16 @@ class OpportunityPolicy < ApplicationPolicy
   end
 
   def update?
-    administrator? || assignee? || conversation_access?
+    @account_user.present?
   end
 
   def destroy?
-    administrator? || assignee? || conversation_access?
-  end
-
-  private
-
-  def administrator?
-    @account_user.administrator?
-  end
-
-  def assignee?
-    record.assignee_id == user.id
-  end
-
-  def conversation_access?
-    return false if record.origin_conversation.blank?
-
-    Pundit.policy!(user_context, record.origin_conversation).show?
+    @account_user.present?
   end
 
   class Scope < Scope
     def resolve
-      return scope.where(account_id: account.id) if @account_user.administrator?
-
-      inbox_ids = user.inboxes.where(account_id: account.id).select(:id)
-      team_ids = user.teams.where(account_id: account.id).select(:id)
-
       scope.where(account_id: account.id)
-           .left_outer_joins(:origin_conversation)
-           .where(
-             'ichatr_opportunities.assignee_id = :user_id OR ' \
-             'conversations.inbox_id IN (:inbox_ids) OR ' \
-             'conversations.team_id IN (:team_ids)',
-             user_id: user.id,
-             inbox_ids: inbox_ids,
-             team_ids: team_ids
-           )
     end
   end
 end
