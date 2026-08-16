e61908761bf21986491f59a92f026a8decee4e80
Edney Matias <edneymatias@gmail.com>
2026-08-16 19:52:51 +0000

    fix(broadcasts): broadcast opportunity_updated over action cable on opportunity_created

--- custom/app/listeners/custom/action_cable_listener.rb
+++ custom/app/listeners/custom/action_cable_listener.rb
@@ -1,5 +1,17 @@
+# frozen_string_literal: true
+
 module Custom::ActionCableListener
+  def opportunity_created(event)
+    broadcast_opportunity(event)
+  end
+
   def opportunity_updated(event)
+    broadcast_opportunity(event)
+  end
+
+  private
+
+  def broadcast_opportunity(event)
     opportunity = event.data[:opportunity]
     ActionCableBroadcastJob.perform_later(["account_#{opportunity.account_id}"], 'opportunity_updated', opportunity.as_json)
   end
